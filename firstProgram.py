78ebdb2e7c42d02cdc6a678510e62d394b875c49
pythonprogrammerRaji <rajeshwarirajeshwari34879@gmail.com>
2026-01-16 22:51:16 +0530

    upload the  new file

---
+++ firstProgram.py
@@ -0,0 +1,84 @@
+# python some program.py to print
+print("Welcome to Python")
+
+print("This is our first Session")
+
+# Creating variables
+
+Numvar = 9 #int value
+print(Numvar)
+
+Num2var = 8.6    #Float value
+print(Num2var)
+
+Num3var = 34500
+print(Num3var)
+
+Num1var = "Pyhton Basis"
+print(Num1var)
+
+Num2var = "First Session"
+print(Num2var)
+
+# some rules of variavle
+#-> Staring variable not allowed to numbers
+# -> certain character have
+# Alphabets (A-Z),(a-z),
+Name = "Raji"
+print(Name)
+
+# Numbers (0-9)
+a = 3
+print(a)
+
+# special character Underscore (_)
+nam_var = "This python Program"
+print(nam_var)
+
+# variable is case Sensitive
+var = "Aishu"
+Var = "Vani"
+print(var)
+print(Var)
+
+
+# List -> we can store all data types value in one time
+
+List = ["a", 34, "Aishu", 34.56]
+print(List)
+
+ListNum = []
+ListNum.append("a")
+ListNum.append(5)
+ListNum.append(3450)
+ListNum.append(80.5)
+ListNum.append("Python")
+ListNum.append("Java")
+print(ListNum)
+
+#Create User Input
+# varname = input("Please neter your name:")
+# varage = input("Please enter your age:")
+#
+# print("Your name is",varname)
+# print("You are age is,",varage)
+
+# a = int(input("Enter first value: "))
+# b =int(input("Enter Second value: "))
+# sum = a+b
+
+# print("The First Value is", a)
+# print("The Second value is", b)
+# print("The Third value is", c)
+
+# print("The Sum of Three Numbers is", sum)
+
+varnum1 = int(input("Enter First Number: "))
+varnum2 = int(input("Enter Second Number : "))
+varnum3 = int(input("Enter Third Number : "))
+Product = varnum1*varnum2*varnum3
+
+print("The First Number is", varnum1)
+print("The First Number is", varnum2)
+print("The First Number is", varnum3)
+print("The Product of Three Numbers are :", Product)
